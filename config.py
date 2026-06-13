9382b7986363dc2e6192ca5538a5d2c8124a12c0
SandervanU <roncallisander@gmail.com>
2026-06-13 13:36:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 from pathlib import Path
+import shutil
 
+# bronbestand in je repo (read-only backup)
+SOURCE = Path(__file__).resolve().parent / "data.xlsx"
+
+# runtime bestand (hier wordt alles in geschreven)
 DATA = Path("/tmp/data.xlsx")
+
+def init_data():
+    if not DATA.exists():
+        shutil.copy(SOURCE, DATA)
